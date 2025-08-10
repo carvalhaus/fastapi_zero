927ef68c19bd172bdc36193e30017e1b107ce01a
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-08-10 19:54:02 -0300

    add more routers and tests for users

--- fastapi_zero/routers/user_router.py
+++ fastapi_zero/routers/user_router.py
@@ -1,9 +1,9 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from ..database.database import database
-from ..schemas.user_schema import UserDB, UserPublic, UserSchema
+from ..schemas.user_schema import UserDB, UserList, UserPublic, UserSchema
 
 router = APIRouter(prefix='/users', tags=['Users'])
 
@@ -14,3 +14,48 @@ def create_user(user: UserSchema):
 
     database.append(user_with_id)
     return user_with_id
+
+
+@router.get('/', status_code=HTTPStatus.OK, response_model=UserList)
+def get_users():
+    return {'users': database}
+
+
+@router.put(
+    '/{user_id}/', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Usuário não encontrado!'
+        )
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@router.get(
+    '/{user_id}/', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def get_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Usuário não encontrado!'
+        )
+
+    return database[user_id - 1]
+
+
+@router.delete(
+    '/{user_id}/', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Usuário não encontrado!'
+        )
+
+    return database.pop(user_id - 1)
--- tests/routers/test_user_router.py
+++ tests/routers/test_user_router.py
@@ -17,3 +17,86 @@ def test_create_user(client):
         'username': 'alice',
         'email': 'alice@example.com',
     }
+
+
+def test_get_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'alice',
+                'email': 'alice@example.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': '123456789',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/99999',
+        json={
+            'username': 'not_found',
+            'email': 'not_found@example.com',
+            'password': '123456789',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Usuário não encontrado!'}
+
+
+def test_get_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+
+
+def test_get_user_not_found(client):
+    response = client.get('/users/99999')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Usuário não encontrado!'}
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/99999')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Usuário não encontrado!'}
