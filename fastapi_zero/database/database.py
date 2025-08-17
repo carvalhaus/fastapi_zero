91f9e8f6c6b04098cce32dfc3853cc6e263c8591
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-08-17 19:15:19 -0300

    create session in database

--- fastapi_zero/database/database.py
+++ fastapi_zero/database/database.py
@@ -1,5 +1,16 @@
-from sqlalchemy.orm import registry
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, registry
+
+from fastapi_zero.settings import Settings
 
 table_registry = registry()
+
+engine = create_engine(Settings().DATABASE_URL)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 database = []
