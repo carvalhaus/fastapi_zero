88f605be8c519aa18e44595316fbe12ea5dbc79d
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-08-17 14:35:49 -0300

    add updated_at in User

--- tests/database/test_db.py
+++ tests/database/test_db.py
@@ -24,4 +24,5 @@ def test_create_user(session, mock_db_time):
         'email': 'alice@example.com',
         'password': '123456789',
         'created_at': time,
+        'updated_at': time,
     }
