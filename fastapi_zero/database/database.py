7700be9b3d43d554ed6dfe445912da3ae83bf08f
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-09-13 20:07:29 -0300

    set uncover for get_session, which will not be called in tests

--- fastapi_zero/database/database.py
+++ fastapi_zero/database/database.py
@@ -8,6 +8,6 @@ table_registry = registry()
 engine = create_async_engine(Settings().DATABASE_URL)
 
 
-async def get_session():
+async def get_session():  # pragma: no cover
     async with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
